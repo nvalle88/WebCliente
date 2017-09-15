a57e9c34f2b084391a75bafafbfecf5695890d12
Brian Molina <bmolina@digitalstrategy.com.ec>
2017-09-15 10:14:08 -0500

    Comentario constante

--- WebCliente/ds.WebCliente.entidades/Constantes/Class1.cs
+++ WebCliente/ds.WebCliente.entidades/Constantes/Class1.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ds.WebCliente.entidades.Constantes
 {
+    /*
+ Creado por DigitalStrategy
+ La clase constante, es una clase en la cual se deben agregar todas la informacion que sera constante para el proyecto
+ */
     class Class1
     {
     }
